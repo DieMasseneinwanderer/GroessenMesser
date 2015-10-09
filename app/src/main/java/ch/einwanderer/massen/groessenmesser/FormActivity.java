db7b318995b1e99b3007f616b05edd45ed9ba721
waleed <waleed@4eyes.ch>
2015-10-09 20:09:54 +0200

    groessenmessung implementieren

---
+++ app/src/main/java/ch/einwanderer/massen/groessenmesser/FormActivity.java
@@ -0,0 +1,119 @@
+package ch.einwanderer.massen.groessenmesser;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FormActivity extends AppCompatActivity {
+
+    private double upperAngel;
+    private double lowerAngel;
+    private double distance;
+    private double size;
+
+    private EditText upperEdit;
+    private EditText lowerEdit;
+    private EditText distanceEdit;
+    private EditText sizeEdit;
+
+    private final int SCAN_QR_CODE = 159;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+        Intent intent = getIntent();
+        lowerAngel = intent.getDoubleExtra("lower", 0.0);
+        upperAngel = intent.getDoubleExtra("upper", 0.0);
+
+        upperEdit = (EditText) findViewById(R.id.upper);
+        lowerEdit = (EditText) findViewById(R.id.lower);
+        distanceEdit = (EditText) findViewById(R.id.distance);
+        sizeEdit = (EditText) findViewById(R.id.size);
+
+        upperEdit.setText(String.format("%.1f", upperAngel));
+        lowerEdit.setText(String.format("%.1f", lowerAngel));
+
+        distanceEdit.requestFocus();
+        distanceEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    distance = Double.parseDouble(distanceEdit.getText().toString());
+                    calculateSize();
+                    sizeEdit.setText(String.format("%.1f", size));
+                }
+            }
+        });
+
+        Button logButton = (Button) findViewById(R.id.log);
+        logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateSize();
+                sizeEdit.setText(String.format("%.1f", size));
+
+                Intent i = new Intent("com.google.zxing.client.android.SCAN");
+
+                if (getPackageManager().queryIntentActivities(i, PackageManager.MATCH_ALL).isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "QR scan App not Installed", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                i.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                startActivityForResult(i, SCAN_QR_CODE);
+
+
+
+            }
+        });
+
+    }
+
+    private void calculateSize() {
+        double betweenAngel = upperAngel - lowerAngel;
+        double hypotenuse = distance / Math.sin(Math.toRadians(lowerAngel));
+        double oppositeAngel = 180 - (betweenAngel + lowerAngel);
+        size =  hypotenuse * Math.sin(Math.toRadians(betweenAngel)) / Math.sin(Math.toRadians(oppositeAngel));
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultIntent) {
+        if (requestCode == SCAN_QR_CODE && resultCode == RESULT_OK) {
+            Intent logIntent = new Intent("ch.appquest.intent.LOG");
+
+            if (getPackageManager().queryIntentActivities(logIntent, PackageManager.MATCH_ALL).isEmpty()) {
+                Toast.makeText(this, "Logbook App not Installed", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            // Achtung, je nach App wird etwas anderes eingetragen
+            String logmessage = Double.toString(size);
+            JSONObject jsonLog = new JSONObject();
+            try {
+                jsonLog.put("task", "Groessenmesser");
+                jsonLog.put("object", resultIntent.getStringExtra("SCAN_RESULT"));
+                jsonLog.put("height", logmessage);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            logIntent.putExtra("ch.appquest.logmessage", jsonLog.toString());
+            startActivity(logIntent);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/ch/einwanderer/massen/groessenmesser/MainActivity.java
+++ app/src/main/java/ch/einwanderer/massen/groessenmesser/MainActivity.java
@@ -1,23 +1,40 @@
 package ch.einwanderer.massen.groessenmesser;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private Camera mCamera;
     private CameraPreview mPreview;
+    private TextView mWinkel;
+
+    private final float[] magneticFieldData = new float[3];
+    private final float[] accelerationData = new float[3];
+
+    private double currentRotation;
+    private double lowerAngel;
+    private boolean hasClicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +49,38 @@ public class MainActivity extends AppCompatActivity {
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
         preview.addView(mPreview);
         getSupportActionBar().hide();
+
+        mWinkel = (TextView) findViewById(R.id.winkel);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+
+        findViewById(R.id.layout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onActivityClick();
+            }
+        });
+    }
+
+    private void onActivityClick() {
+
+        if(!hasClicked) {
+            hasClicked = true;
+            lowerAngel = currentRotation;
+
+        } else {
+
+            Intent intent = new Intent(this, FormActivity.class);
+            intent.putExtra("lower", lowerAngel);
+            intent.putExtra("upper", currentRotation);
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -74,17 +123,46 @@ public class MainActivity extends AppCompatActivity {
         mCamera.release();
     }
 
-    /** Check if this device has a camera */
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerationData, 0, 3);
         }
+
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magneticFieldData, 0, 3);
+        }
+
+        currentRotation = getCurrentRotationValue();
+
+
+
+        mWinkel.setText("Winkel: " + String.format("%.1f", currentRotation));
+
     }
 
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    private double getCurrentRotationValue() {
+        float[] rotationMatrix = new float[16];
+
+        if (SensorManager.getRotationMatrix(rotationMatrix, null, accelerationData, magneticFieldData)) {
+
+            float[] orientation = new float[4];
+            SensorManager.getOrientation(rotationMatrix, orientation);
+
+            double neigung = Math.toDegrees(orientation[2]);
+
+            return Math.abs(neigung);
+        }
+
+        return 0;
+    }
 
 
 }
